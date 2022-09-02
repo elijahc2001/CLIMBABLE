6e1a7e9f05317caaec4567c625e57c0980b59f3c
elijahc2001 <elicholmondeley@gmail.com>
2022-09-02 14:55:05 +1200

    updates

--- run.py
+++ run.py
@@ -2,8 +2,15 @@ from flask import Flask, render_template
 import sqlite3
 app = Flask(__name__)
 
-CLIMBDATABASE = 'CLIMBABLE_DATABASE'
+CLIMBDATABASE = 'CLIMBABLE_DATABASE.db'
 
 @app.route('/')
 def index():
       return render_template ("index.html")
+
+def get_all(db):
+    pokemon = []
+    cur = db.execute('SELECT * FROM pokemon_cleaned')
+    for row in cur:
+        pokemon.append(list(row))
+    return {'pokemon':pokemon}
